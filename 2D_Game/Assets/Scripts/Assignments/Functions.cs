a5ee6e7187669ff492352ce1e5c6fa22331b0d86
Laura_Loveland <loveland.lulu@Gmail.com>
2019-02-12 14:35:58 -0700

    functions and Playercontroller

---
+++ 2D_Game/Assets/Scripts/Assignments/Functions.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Functions : MonoBehaviour{
+    public string weather;
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
---
+++ 2D_Game/Assets/Scripts/Assignments/Playercontroller.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Playercontroller : MonoBehaviour
+{ public float moveSpeed;
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    { if (Input.GetKey(KeyCode.D))
+           GetConmponent<Rigidbody2D>().Velocity
+        
+    }
+}
