212a7d533a980fad199c227319613628d53071b1
Laura_Loveland <loveland.lulu@Gmail.com>
2019-02-13 16:12:03 -0700

    Functions

--- 2D_Game/Assets/Scripts/Assignments/Functions.cs
+++ 2D_Game/Assets/Scripts/Assignments/Functions.cs
@@ -4,13 +4,14 @@ using UnityEngine;
 
 public class Functions : MonoBehaviour{
     public string weather;
-    
+
     // Start is called before the first frame update
     void Start()
     {
-        
-    }
+        weather(waether);
 
+    }
+          void
     // Update is called once per frame
     void Update()
     {
