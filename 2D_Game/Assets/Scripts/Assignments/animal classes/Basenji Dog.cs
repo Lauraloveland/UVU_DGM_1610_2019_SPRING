58e0a8d9802d732e6d3db282d6af1bb10984c821
Laura_Loveland <loveland.lulu@Gmail.com>
2019-02-14 00:10:11 -0700

    Class Inheritance

---
+++ 2D_Game/Assets/Scripts/Assignments/animal classes/Basenji Dog.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BasenjiDog : Canine
+{
+    public bool wrinkles;
+    // Start is called before the first frame update
+    void Start()
+    {print ("Bark");
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
