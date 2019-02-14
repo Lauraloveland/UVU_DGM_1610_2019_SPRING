709da8894703c1cf1267ebee1f883e41e146c590
Laura_Loveland <loveland.lulu@Gmail.com>
2019-02-14 12:58:30 -0700

    Class Inheritance

---
+++ 2D_Game/Assets/Scripts/Assignments/animal classes/Canine.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Canine : Animal{
+
+    public bool domesticated;
+    public bool curlyTail;
+    public bool pointedEars;
+    public bool shortHair;
+    public float size;
+    public float weight;
+    public float agility;
+    public string furColor;
+    public int age;
+
+
+   
