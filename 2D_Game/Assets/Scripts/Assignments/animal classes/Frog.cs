67bc2e73338d2f433ab5bc6198a91193544c4433
Laura_Loveland <loveland.lulu@Gmail.com>
2019-02-19 10:30:50 -0700

    Inherited class

--- 2D_Game/Assets/Scripts/Assignments/animal classes/Frog.cs
+++ 2D_Game/Assets/Scripts/Assignments/animal classes/Frog.cs
@@ -2,11 +2,13 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class Frog : Amphibian
+public class Frog: Amphibian
 {
 public bool green;
 public bool size;
 public bool eyeColor;
+public bool weight;
+public int 
 
 
 }
