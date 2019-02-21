070c3507d8d63e0287add4c8c7365e1d51431187
Laura_Loveland <loveland.lulu@Gmail.com>
2019-02-20 17:06:40 -0700

    Inherited Class

--- 2D_Game/Assets/Scripts/Assignments/animal classes/Basenji Dog.cs
+++ 2D_Game/Assets/Scripts/Assignments/animal classes/Basenji Dog.cs
@@ -4,7 +4,7 @@ using UnityEngine;
 
 public class BasenjiDog : Canine
 {
-    public bool curlyTail;
+    public bool age;
     // Start is called before the first frame update
     void Start()
     {print ("Bark");
