3abbdad2338d8333f9830f12578efc110a74b3d2
Laura_Loveland <loveland.lulu@Gmail.com>
2019-02-20 17:06:30 -0700

    coin pickup edited

---
+++ 2D_Game/Assets/Scripts/Assignments/coin pickup.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class coinpickup : MonoBehaviour
+{
+
+public int coinValue;
+void OnTriggerEnter2D(Collider2D other){
+    if (other.name == "player"){
+            print ("You've Collected The Coin!");
+            Destroy(gameObject);
+        }
+
+    }         
+
+}
+
+
+
