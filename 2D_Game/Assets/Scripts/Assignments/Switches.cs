b8ad8681d6a66cb5922a3d987b60f6fafe07e34d
Laura_Loveland <loveland.lulu@Gmail.com>
2019-02-27 16:59:20 -0700

    Switches

---
+++ 2D_Game/Assets/Scripts/Assignments/Switches.cs
@@ -0,0 +1,61 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Switches : MonoBehaviour{
+
+
+public string suspect;
+public string weapon;
+
+    void Start() 
+    {
+    
+     MurderMystery(suspect);
+        
+    }
+      void Update()
+    {
+        
+    }
+
+void MurderMystery (string person){
+
+      switch(person){
+
+            case "Mr. Ketchup":
+        
+
+            print("Im innocent, Ya'll Ugly!");
+
+            break;
+
+            case "Mr. Radish":
+
+            print("I am also innocent");
+
+            break;
+
+            case "Mrs. Fry sauce":
+
+            print("I was on the phone");
+
+            break;
+
+            case "Mrs. Mayo":
+
+            print ("whatever man I gave up Years ago");
+
+            break;
+
+            default:
+
+            print("I am not familiar with "+suspect+"!");
+         
+            break;
+
+          }
+
+         
+          
+      }
