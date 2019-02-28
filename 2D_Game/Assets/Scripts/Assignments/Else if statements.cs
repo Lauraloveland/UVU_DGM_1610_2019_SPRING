343713c35256e8794a500c7f0bd7f19f58ec24bd
Laura_Loveland <loveland.lulu@Gmail.com>
2019-02-28 00:07:25 -0700

    updated else if statement

---
+++ 2D_Game/Assets/Scripts/Assignments/Else if statements.cs
@@ -0,0 +1,49 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Elseifstatements : MonoBehaviour{
+
+
+
+public string noodles;
+public bool spicy = true;
+public string sauce;
+public string beef;
+public string broth;
+public bool boiled;
+
+
+
+
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+      
+        if (noodles == "tender"){
+            if (spicy){
+                print ("The noodles are now done");
+        }
+
+          else if (noodles == "soggy" && boiled == true);{
+
+         }
+          print("This is sludge!");
+
+        }
+
+        else if (noodles == "plain" && spicy == false){
+            print ("okay, still gross");
+
+        }
+    }
+
+
+    // Update is called once per frame
+    void Update(){
+        
+    }
+    
+        
+    
