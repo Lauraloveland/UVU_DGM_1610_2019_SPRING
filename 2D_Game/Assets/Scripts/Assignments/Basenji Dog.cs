3dc56de2d9b2e691f1cacd0790d837327cfec752
Laura_Loveland <loveland.lulu@Gmail.com>
2019-02-28 00:05:11 -0700

    animal class updated

--- 2D_Game/Assets/Scripts/Assignments/Basenji Dog.cs
+++ 2D_Game/Assets/Scripts/Assignments/Basenji Dog.cs
@@ -4,7 +4,7 @@ using UnityEngine;
 
 public class BasenjiDog : Canine
 {
-    public bool age;
+    public bool height;
     // Start is called before the first frame update
     void Start()
     {print ("Bark");
