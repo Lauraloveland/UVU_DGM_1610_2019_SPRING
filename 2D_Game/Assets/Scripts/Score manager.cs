49a6cbf2124dfb6b654c1f65024d20c4bcadda37
Laura_Loveland <loveland.lulu@Gmail.com>
2019-03-11 16:32:33 -0600

    Score manager

---
+++ 2D_Game/Assets/Scripts/Score manager.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Scoremanager : MonoBehaviour
+{
+    public static int score;
+    Text ScoreText;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        ScoreText = GetComponent<Text>();
+        score = 0;
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(score<0)
+        score=0;
+
+        ScoreText.text = " " + score;
+    }
+}
