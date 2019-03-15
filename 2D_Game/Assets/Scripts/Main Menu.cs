113775f86d2cfa390f98d4081d71f74bd8a6d1e7
Laura_Loveland <loveland.lulu@Gmail.com>
2019-03-15 01:01:40 -0600

    Main Menu edited

---
+++ 2D_Game/Assets/Scripts/Main Menu.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.ScoreManagment; 
+public class MainMenu : MonoBehaviour{
+    public int LevelToLoad;
+    
+    public void loadLevel(){
+        Scoremanager.LoadScene(LevelToLoad);
+
+    }
+
+    public void LevelExit(){
+        Application.Quit();
+
+    }
+}
+
+
+
+
