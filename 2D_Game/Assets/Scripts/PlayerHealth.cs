0f4856d87ac8b80ce49dd44891b79cbfa2ad2986
Laura_Loveland <loveland.lulu@Gmail.com>
2019-03-16 00:44:09 -0600

    Player health currently being worked on

---
+++ 2D_Game/Assets/Scripts/PlayerHealth.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections;
+using UnityEngine.UI;
+using UnityEngine;
+
+public class PlayerHealth : MonoBehaviour
+{
+   public int startingHealth = 100;
+   public int curentHealth;
+    
+}
+
+
