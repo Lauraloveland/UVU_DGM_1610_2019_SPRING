76d391f7399d3d073727189280e0af885753dc7f
Laura_Loveland <loveland.lulu@Gmail.com>
2019-03-18 00:04:21 -0600

    Practice animation loop for coin

---
+++ 2D_Game/Assets/Scripts/Animation loop (object).cs
@@ -0,0 +1,15 @@
+﻿ 
+using UnityEngine;
+using System.Collections;
+ 
+public class TestScript : MonoBehaviour
+{
+    public AnimationCurve myCurve;
+   
+    void Update()
+    {
+        transform.position = new Vector3(transform.position.x, myCurve.Evaluate((Time.time % myCurve.length)), transform.position.z);
+    }
+}
+//  Heavily referenced from an online unity forum
+// This is a test to see if I can animate coins to move up and down
