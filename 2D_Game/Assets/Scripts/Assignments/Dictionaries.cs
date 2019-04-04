5fc71b5fd8ba74ca373b12b36ef4f3ed8ed67104
Laura_Loveland <loveland.lulu@Gmail.com>
2019-04-04 00:54:11 -0600

    Forgot to stage the dictionary file sorry

---
+++ 2D_Game/Assets/Scripts/Assignments/Dictionaries.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Dictionaries : MonoBehaviour
+{
+    // Start is called before the first frame update
+    public Mashtable personalDetails = new Mashtable();
+    
+
+    
+
+    void Start()
+    {
+        personalDetails.Add("firstName", "Greg");
+        personalDetails.Add("Lastname","Yeet");
+        personalDetails.Add()
+
+
+        // Five seperate hashtables
+
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
