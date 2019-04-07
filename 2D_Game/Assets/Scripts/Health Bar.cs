bdf8d256668d4ab8187a1c6937fbe72bcb06e458
Laura_Loveland <loveland.lulu@Gmail.com>
2019-04-06 21:53:37 -0600

    Health bar updated 4.6.2019

--- 2D_Game/Assets/Scripts/Health Bar.cs
+++ 2D_Game/Assets/Scripts/Health Bar.cs
@@ -1,18 +1,35 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-
+using UnityEngine.UI;
 public class HealthBar : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    public Image ImgHealthBar;
+    public Text TxtHealth;
+    public int Min;
+    public int Max;
+
+    private int mCurrentValue;
+    private float mCurrentPercent;
+    public void SetHealth(int health)
     {
-        
+        if(health ! =mCurrentValue)
+        {
+            if(Max - Min == 0)
+            {
+               mCurrentPercent = 0;
+               mCurrentValue = 0; 
+               {
+                   else 
+                   {
+                       mCurrentValue = health;
+                   }
+               }
+            }
+        }
     }
 
-    // Update is called once per frame
-    void Update()
-    {
-        
-    }
-}
+
+
+
+
