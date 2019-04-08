a24996d84c80393e8a842684d74cc77c194d09e7
Laura_Loveland <loveland.lulu@Gmail.com>
2019-04-07 19:49:03 -0600

    Health bar restarted and errors are currently being fixed

--- 2D_Game/Assets/Scripts/Health Bar.cs
+++ 2D_Game/Assets/Scripts/Health Bar.cs
@@ -1,33 +1,34 @@
-﻿using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
-using UnityEngine.UI;
-public class HealthBar : MonoBehaviour
-{
-    public Image ImgHealthBar;
-    public Text TxtHealth;
-    public int Min;
-    public int Max;
+﻿// using System.Collections;
+// using System.Collections.Generic;
+// using UnityEngine;
+// using UnityEngine.UI;
+// public class HealthBar : MonoBehaviour
+// {
+//     public Image ImgHealthBar;
+//     public Text TxtHealth;
+//     public int Min;
+//     public int Max;
 
-    private int mCurrentValue;
-    private float mCurrentPercent;
-    public void SetHealth(int health)
-    {
-        if(health ! =mCurrentValue)
-        {
-            if(Max - Min == 0)
-            {
-               mCurrentPercent = 0;
-               mCurrentValue = 0; 
-               {
-                   else 
-                   {
-                       mCurrentValue = health;
-                   }
-               }
-            }
-        }
-    }
+//     private int mCurrentValue;
+//     private float mCurrentPercent;
+//     public void SetHealth(int health)
+//     {
+//         if(health = mCurrentValue)
+//         {
+//             if(Max - Min == 0)
+//             {
+//                mCurrentPercent = 0;
+//                mCurrentValue = 0; 
+//                {
+                  
+//                    else 
+//                    {
+//                        mCurrentValue = health;
+//                    }
+//                }
+//             }
+//         }
+//     }
 
 
 
---
+++ 2D_Game/Assets/Scripts/NewHealthBar.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class NewHealthBar : MonoBehaviour
+{
+    private Transform bar;
+    // Start is called before the first frame update
+   private void Start()
+    {
+        Transform bar = transform.Find("Bar");
+        bar.localScale = new Vector3(-4f,1f);
+
+    }
+ public void SetSize(float sizeNormalized){
+     bar.localScale = new Vector3(sizeNormalized, 1f);
+
+ }
+  
+}
