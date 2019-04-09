ddf393798571db56ef5be40759c02d98b65b8931
Laura_Loveland <loveland.lulu@Gmail.com>
2019-04-09 11:04:32 -0600

    Projectile created in class and edited

---
+++ 2D_Game/Assets/Scripts/projectile.cs
@@ -0,0 +1,49 @@
+﻿// using System.Collections;
+// using UnityEngine;
+// public class projectile : MonoBehaviour {
+//     public float speed;
+//     public float timeOut;
+//     public GameObject player;
+//     public GameObject enemyDeath;
+//     public GameObject projectileParticle;
+//     public int pointsForKill;
+
+// }
+
+// void Start(){
+
+//     player = GameObject.Find("Player");
+
+//     enemyDeath = Resources.Load("Prefabs/Death_PS") as GameObject;
+
+// projectileParticle = Resources.Load("Prefabs/Respawn_PS") as GameObject;
+// if(PC.Transform.localScale.x < 0)
+// Speed = - Speed;
+
+// Destroy(GameObject,timeOut);
+
+
+// }
+
+// void Update (){
+//     GetComponent<Rigidbody2D>().velocity = new Vector2(Speed, GetComponent<Rigidbody2D>()velocity.y);
+// }
+// void OnTriggerenter2D(Collider2D other ){
+    
+//     if(other.tag == "Enemy"){
+//         Instantiate(enemyDeath. other.Transform.position, other.transform.rotation);
+//         Destroy (other.gameObject);
+//         Scoremanager.AddPoints (pointsForKill);
+
+//     }
+//     Destroy(GameObject);
+// }
+// void OnCollisionEnter2D(Collision2D other)
+// {
+    
+//         Instantaite(projectileParticle, Transform.position, Transform.rotation);
+//         Destroy (GameObject);
+
+//     }
+// }
+
