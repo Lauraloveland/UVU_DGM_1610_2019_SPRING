8b2597e63d9f218002fb4303a0a0a046782d7408
Laura_Loveland <loveland.lulu@Gmail.com>
2019-04-16 00:30:23 -0600

    player controller edited with double jump

--- 2D_Game/Assets/Scripts/Playercontroller.cs
+++ 2D_Game/Assets/Scripts/Playercontroller.cs
@@ -13,6 +13,7 @@ private bool grounded;
 public Transform groundCheck;
 public float groundCheckRadius;
 public LayerMask whatIsGround;
+public Animator Animator;
 
     // Start is called before the first frame update
     void Start()
@@ -29,29 +30,43 @@ void FixedUpdate(){
         if (Input.GetKey(KeyCode.D))
         {
             GetComponent<Rigidbody2D>().velocity = new Vector2(moveSpeed, 0);
+            Animator.SetBool("isWalking", true);
+        }
+        else if (Input.GetKeyUp(KeyCode.D)){
+            Animator.SetBool("isWalking", false);
+
         }
 
         if (Input.GetKey(KeyCode.A))
         {
             GetComponent<Rigidbody2D>().velocity = new Vector2(-moveSpeed, 0);
+            Animator.SetBool("isWalking", true);
+        }
+        else if (Input.GetKeyUp (KeyCode.A)){
+            Animator.SetBool("isWalking", false);
         }
 
-        if (Input.GetKey(KeyCode.Space))
+         if (Input.GetKey(KeyCode.Space))
         {
             GetComponent<Rigidbody2D>().velocity = new Vector2(0, moveSpeed);
         }
 
-
+       if(grounded){
+            // doubleJump = false;
+            // Animator.SetBool("isJumping", false);
+       }
 
         // player jump
         if(Input.GetKeyDown(KeyCode.W)&& grounded){
+            Jump ();
             
 
         }
     }
            
-        void Jump(){
+        public void Jump(){
             GetComponent<Rigidbody2D>().velocity = new Vector2(GetComponent<Rigidbody2D>().velocity.x,jumpHeight);
+            Animator.SetBool("isJumping", true);
         }
     
 }
