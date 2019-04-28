4c7961759fc8fc5725aa429b219118526ee5c6c2
Laura_Loveland <loveland.lulu@Gmail.com>
2019-04-28 10:11:32 -0600

    Edits made in game, art added, code edited

--- 2D_Game/Assets/Scripts/YouWin.cs
+++ 2D_Game/Assets/Scripts/YouWin.cs
@@ -1,15 +1,91 @@
-﻿using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
+﻿using UnityEngine;
+using UnityEngine.UI;
+using System.Collections;
 
-//put this script on an empty gameobject with a box collider with the "is trigger" box checked
-public class YouWin : MonoBehaviour
-{
-    //create a variable for your you win panel (see unity's documentation on UI panels)
+public class YouWin : MonoBehaviour {
 
-    void OnTriggerEnter()
+    public Text[] buttonList;
+    public GameObject gameOverPanel;
+    public Text gameOverText;
+
+    private string playerSide;
+
+    void Awake ()
     {
-        //set the you win panel variable active (see unity documentation on GameObject.SetActive())
+        SetGameControllerReferenceOnButtons();
+        playerSide = "X";
+        gameOverPanel.SetActive(false);
     }
 
+    void SetGameControllerReferenceOnButtons ()
+    {
+        for (int i = 0; i < buttonList.Length; i++)
+        {
+      
+        }
+    }
+
+    public string GetPlayerSide ()
+    {
+        return playerSide;
+    }
+
+    public void EndTurn ()
+    {
+
+        if (buttonList [0].text == playerSide && buttonList [1].text == playerSide && buttonList [2].text == playerSide)
+        {
+            GameOver();
+        }
+
+        if (buttonList [3].text == playerSide && buttonList [4].text == playerSide && buttonList [5].text == playerSide)
+        {
+            GameOver();
+        }
+
+        if (buttonList [6].text == playerSide && buttonList [7].text == playerSide && buttonList [8].text == playerSide)
+        {
+            GameOver();
+        }
+
+        if (buttonList [0].text == playerSide && buttonList [3].text == playerSide && buttonList [6].text == playerSide)
+        {
+            GameOver();
+        }
+
+        if (buttonList [1].text == playerSide && buttonList [4].text == playerSide && buttonList [7].text == playerSide)
+        {
+            GameOver();
+        }
+
+        if (buttonList [2].text == playerSide && buttonList [5].text == playerSide && buttonList [8].text == playerSide)
+        {
+            GameOver();
+        }
+
+        if (buttonList [0].text == playerSide && buttonList [4].text == playerSide && buttonList [8].text == playerSide)
+        {
+            GameOver();
+        }
+        if (buttonList [2].text == playerSide && buttonList [4].text == playerSide && buttonList [6].text == playerSide)
+        {
+            GameOver();
+        }
+        ChangeSides();
+    }
+
+    void ChangeSides ()
+    {
+        playerSide = (playerSide == "X") ? "O" : "X";
+    }
+
+    void GameOver ()
+    {
+        for (int i = 0; i < buttonList.Length; i++)
+        {
+            buttonList[i].GetComponentInParent<Button>().interactable = false;
+        }
+        gameOverPanel.SetActive(true);
+        gameOverText.text = playerSide + " Wins!";
+    }
 }
